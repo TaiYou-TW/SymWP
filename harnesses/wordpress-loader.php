ebdd88ddcde9afc4aa14e777f0ee32b9b68c87fd
Shang-Hung Wan <shang-hung.wan@rwth-aachen.de>
2025-04-24 16:55:53 +0200

    feat: add require `rest-api/class-wp-rest-request.php` in wordpress-loader

--- harnesses/wordpress-loader.php
+++ harnesses/wordpress-loader.php
@@ -1,140 +1,145 @@
 <?php
 
-function my_wp_initial_constants() {
-	global $blog_id;
+function my_wp_initial_constants()
+{
+    global $blog_id;
 
-	/**#@+
-	 * Constants for expressing human-readable data sizes in their respective number of bytes.
-	 *
-	 * @since 4.4.0
-	 * @since 6.0.0 `PB_IN_BYTES`, `EB_IN_BYTES`, `ZB_IN_BYTES`, and `YB_IN_BYTES` were added.
-	 */
-	define( 'KB_IN_BYTES', 1024 );
-	define( 'MB_IN_BYTES', 1024 * KB_IN_BYTES );
-	define( 'GB_IN_BYTES', 1024 * MB_IN_BYTES );
-	define( 'TB_IN_BYTES', 1024 * GB_IN_BYTES );
-	define( 'PB_IN_BYTES', 1024 * TB_IN_BYTES );
-	define( 'EB_IN_BYTES', 1024 * PB_IN_BYTES );
-	define( 'ZB_IN_BYTES', 1024 * EB_IN_BYTES );
-	define( 'YB_IN_BYTES', 1024 * ZB_IN_BYTES );
-	/**#@-*/
+    /**#@+
+     * Constants for expressing human-readable data sizes in their respective number of bytes.
+     *
+     * @since 4.4.0
+     * @since 6.0.0 `PB_IN_BYTES`, `EB_IN_BYTES`, `ZB_IN_BYTES`, and `YB_IN_BYTES` were added.
+     */
+    define('KB_IN_BYTES', 1024);
+    define('MB_IN_BYTES', 1024 * KB_IN_BYTES);
+    define('GB_IN_BYTES', 1024 * MB_IN_BYTES);
+    define('TB_IN_BYTES', 1024 * GB_IN_BYTES);
+    define('PB_IN_BYTES', 1024 * TB_IN_BYTES);
+    define('EB_IN_BYTES', 1024 * PB_IN_BYTES);
+    define('ZB_IN_BYTES', 1024 * EB_IN_BYTES);
+    define('YB_IN_BYTES', 1024 * ZB_IN_BYTES);
+    /**#@-*/
 
-    define( 'WP_START_TIMESTAMP', microtime( true ) );
+    define('WP_START_TIMESTAMP', microtime(true));
 
-	$current_limit     = ini_get( 'memory_limit' );
+    $current_limit = ini_get('memory_limit');
 
-	// Define memory limits.
-    define( 'WP_MEMORY_LIMIT', $current_limit );
+    // Define memory limits.
+    define('WP_MEMORY_LIMIT', $current_limit);
 
-    define( 'WP_MAX_MEMORY_LIMIT', $current_limit );
+    define('WP_MAX_MEMORY_LIMIT', $current_limit);
 
-	// Set memory limits.
-    ini_set( 'memory_limit', WP_MEMORY_LIMIT );
+    // Set memory limits.
+    ini_set('memory_limit', WP_MEMORY_LIMIT);
 
     $blog_id = 1;
 
-    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' ); // No trailing slash, full paths only - WP_CONTENT_URL is defined further down.
+    define('WP_CONTENT_DIR', ABSPATH . 'wp-content'); // No trailing slash, full paths only - WP_CONTENT_URL is defined further down.
 
-	/*
-	 * Add define( 'WP_DEVELOPMENT_MODE', 'core' ), or define( 'WP_DEVELOPMENT_MODE', 'plugin' ), or
-	 * define( 'WP_DEVELOPMENT_MODE', 'theme' ), or define( 'WP_DEVELOPMENT_MODE', 'all' ) to wp-config.php
-	 * to signify development mode for WordPress core, a plugin, a theme, or all three types respectively.
-	 */
-    define( 'WP_DEVELOPMENT_MODE', '' );
+    /*
+     * Add define( 'WP_DEVELOPMENT_MODE', 'core' ), or define( 'WP_DEVELOPMENT_MODE', 'plugin' ), or
+     * define( 'WP_DEVELOPMENT_MODE', 'theme' ), or define( 'WP_DEVELOPMENT_MODE', 'all' ) to wp-config.php
+     * to signify development mode for WordPress core, a plugin, a theme, or all three types respectively.
+     */
+    define('WP_DEVELOPMENT_MODE', '');
 
-	// Add define( 'WP_DEBUG', true ); to wp-config.php to enable display of notices during development.
-    define( 'WP_DEBUG', false );
+    // Add define( 'WP_DEBUG', true ); to wp-config.php to enable display of notices during development.
+    define('WP_DEBUG', false);
 
-	/*
-	 * Add define( 'WP_DEBUG_DISPLAY', null ); to wp-config.php to use the globally configured setting
-	 * for 'display_errors' and not force errors to be displayed. Use false to force 'display_errors' off.
-	 */
-    define( 'WP_DEBUG_DISPLAY', null );
+    /*
+     * Add define( 'WP_DEBUG_DISPLAY', null ); to wp-config.php to use the globally configured setting
+     * for 'display_errors' and not force errors to be displayed. Use false to force 'display_errors' off.
+     */
+    define('WP_DEBUG_DISPLAY', null);
 
-	// Add define( 'WP_DEBUG_LOG', true ); to enable error logging to wp-content/debug.log.
-    define( 'WP_DEBUG_LOG', false );
+    // Add define( 'WP_DEBUG_LOG', true ); to enable error logging to wp-content/debug.log.
+    define('WP_DEBUG_LOG', false);
 
-    define( 'WP_CACHE', false );
+    define('WP_CACHE', false);
 
-	/*
-	 * Add define( 'SCRIPT_DEBUG', true ); to wp-config.php to enable loading of non-minified,
-	 * non-concatenated scripts and stylesheets.
-	 */
-    define( 'SCRIPT_DEBUG', false );
+    /*
+     * Add define( 'SCRIPT_DEBUG', true ); to wp-config.php to enable loading of non-minified,
+     * non-concatenated scripts and stylesheets.
+     */
+    define('SCRIPT_DEBUG', false);
 
-	/**
-	 * Private
-	 */
-    define( 'MEDIA_TRASH', false );
+    /**
+     * Private
+     */
+    define('MEDIA_TRASH', false);
 
-    define( 'SHORTINIT', false );
+    define('SHORTINIT', false);
 
-	// Constants for features added to WP that should short-circuit their plugin implementations.
-	define( 'WP_FEATURE_BETTER_PASSWORDS', true );
+    // Constants for features added to WP that should short-circuit their plugin implementations.
+    define('WP_FEATURE_BETTER_PASSWORDS', true);
 
-	/**#@+
-	 * Constants for expressing human-readable intervals
-	 * in their respective number of seconds.
-	 *
-	 * Please note that these values are approximate and are provided for convenience.
-	 * For example, MONTH_IN_SECONDS wrongly assumes every month has 30 days and
-	 * YEAR_IN_SECONDS does not take leap years into account.
-	 *
-	 * If you need more accuracy please consider using the DateTime class (https://www.php.net/manual/en/class.datetime.php).
-	 *
-	 * @since 3.5.0
-	 * @since 4.4.0 Introduced `MONTH_IN_SECONDS`.
-	 */
-	define( 'MINUTE_IN_SECONDS', 60 );
-	define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
-	define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
-	define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
-	define( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS );
-	define( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS );
-	/**#@-*/
+    /**#@+
+     * Constants for expressing human-readable intervals
+     * in their respective number of seconds.
+     *
+     * Please note that these values are approximate and are provided for convenience.
+     * For example, MONTH_IN_SECONDS wrongly assumes every month has 30 days and
+     * YEAR_IN_SECONDS does not take leap years into account.
+     *
+     * If you need more accuracy please consider using the DateTime class (https://www.php.net/manual/en/class.datetime.php).
+     *
+     * @since 3.5.0
+     * @since 4.4.0 Introduced `MONTH_IN_SECONDS`.
+     */
+    define('MINUTE_IN_SECONDS', 60);
+    define('HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS);
+    define('DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS);
+    define('WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS);
+    define('MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS);
+    define('YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS);
+    /**#@-*/
 }
 
-function my_wp_set_lang_dir() {
-    define( 'WP_LANG_DIR', WP_CONTENT_DIR . '/languages' );
-    define( 'LANGDIR', 'wp-content/languages' );
+function my_wp_set_lang_dir()
+{
+    define('WP_LANG_DIR', WP_CONTENT_DIR . '/languages');
+    define('LANGDIR', 'wp-content/languages');
 }
 
-function my_wp_plugin_directory_constants() {
-    define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' ); // Full URL - WP_CONTENT_DIR is defined further up.
-    define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' ); // Full path, no trailing slash.
-    define( 'WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins' ); // Full URL, no trailing slash.
-    define( 'PLUGINDIR', 'wp-content/plugins' ); // Relative to ABSPATH. For back compat.
-    define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' ); // Full path, no trailing slash.
-    define( 'WPMU_PLUGIN_URL', WP_CONTENT_URL . '/mu-plugins' ); // Full URL, no trailing slash.
-    define( 'MUPLUGINDIR', 'wp-content/mu-plugins' ); // Relative to ABSPATH. For back compat.
+function my_wp_plugin_directory_constants()
+{
+    define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content'); // Full URL - WP_CONTENT_DIR is defined further up.
+    define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins'); // Full path, no trailing slash.
+    define('WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins'); // Full URL, no trailing slash.
+    define('PLUGINDIR', 'wp-content/plugins'); // Relative to ABSPATH. For back compat.
+    define('WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins'); // Full path, no trailing slash.
+    define('WPMU_PLUGIN_URL', WP_CONTENT_URL . '/mu-plugins'); // Full URL, no trailing slash.
+    define('MUPLUGINDIR', 'wp-content/mu-plugins'); // Relative to ABSPATH. For back compat.
 }
 
-function my_wp_cookie_constants() {
-    define( 'COOKIEHASH', '85613653295dc0889e009cc5f528c28a' ); // some non-sense value
-    define( 'USER_COOKIE', 'wordpressuser_' . COOKIEHASH );
-    define( 'PASS_COOKIE', 'wordpresspass_' . COOKIEHASH );
-    define( 'AUTH_COOKIE', 'wordpress_' . COOKIEHASH );
-    define( 'SECURE_AUTH_COOKIE', 'wordpress_sec_' . COOKIEHASH );
-    define( 'LOGGED_IN_COOKIE', 'wordpress_logged_in_' . COOKIEHASH );
-    define( 'TEST_COOKIE', 'wordpress_test_cookie' );
-    define( 'COOKIEPATH', preg_replace( '|https?://[^/]+|i', '', get_option( 'home' ) . '/' ) );
-    define( 'SITECOOKIEPATH', preg_replace( '|https?://[^/]+|i', '', get_option( 'siteurl' ) . '/' ) );
-    define( 'ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin' );
-    define( 'PLUGINS_COOKIE_PATH', preg_replace( '|https?://[^/]+|i', '', WP_PLUGIN_URL ) );
-    define( 'COOKIE_DOMAIN', '' );
-    define( 'RECOVERY_MODE_COOKIE', 'wordpress_rec_' . COOKIEHASH );
+function my_wp_cookie_constants()
+{
+    define('COOKIEHASH', '85613653295dc0889e009cc5f528c28a'); // some non-sense value
+    define('USER_COOKIE', 'wordpressuser_' . COOKIEHASH);
+    define('PASS_COOKIE', 'wordpresspass_' . COOKIEHASH);
+    define('AUTH_COOKIE', 'wordpress_' . COOKIEHASH);
+    define('SECURE_AUTH_COOKIE', 'wordpress_sec_' . COOKIEHASH);
+    define('LOGGED_IN_COOKIE', 'wordpress_logged_in_' . COOKIEHASH);
+    define('TEST_COOKIE', 'wordpress_test_cookie');
+    define('COOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('home') . '/'));
+    define('SITECOOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('siteurl') . '/'));
+    define('ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin');
+    define('PLUGINS_COOKIE_PATH', preg_replace('|https?://[^/]+|i', '', WP_PLUGIN_URL));
+    define('COOKIE_DOMAIN', '');
+    define('RECOVERY_MODE_COOKIE', 'wordpress_rec_' . COOKIEHASH);
 }
 
-function my_require_wp_db() {
-	global $wpdb;
+function my_require_wp_db()
+{
+    global $wpdb;
 
-	require_once ABSPATH . WPINC . '/class-wpdb.php';
+    require_once ABSPATH . WPINC . '/class-wpdb.php';
 
     require_once WP_CONTENT_DIR . '/db.php';
 }
 
-define( 'ABSPATH',  './WordPress/' );
-define( 'WPINC', 'wp-includes' );
+define('ABSPATH', './WordPress/');
+define('WPINC', 'wp-includes');
 
 require ABSPATH . WPINC . '/version.php';
 require ABSPATH . WPINC . '/compat.php';
@@ -295,7 +300,7 @@ require ABSPATH . WPINC . '/class-wp-application-passwords.php';
 // require ABSPATH . WPINC . '/rest-api.php';
 // require ABSPATH . WPINC . '/rest-api/class-wp-rest-server.php';
 // require ABSPATH . WPINC . '/rest-api/class-wp-rest-response.php';
-// require ABSPATH . WPINC . '/rest-api/class-wp-rest-request.php';
+require ABSPATH . WPINC . '/rest-api/class-wp-rest-request.php';
 // require ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-controller.php';
 // require ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-posts-controller.php';
 // require ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-attachments-controller.php';
