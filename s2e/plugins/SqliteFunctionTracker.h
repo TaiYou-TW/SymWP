5d8814cc41203615a57a6c5ca87c630b344a0b59
TaiYou_TW <sun@taiyou.tw>
2025-05-29 06:14:08 +0800

    feat: SqliteFunctionTracker

---
+++ s2e/plugins/SqliteFunctionTracker.h
@@ -0,0 +1,81 @@
+///
+/// Copyright (C) 2025, TaiYou
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#ifndef S2E_PLUGINS_SQLITEFUNCTIONTRACKER_H
+#define S2E_PLUGINS_SQLITEFUNCTIONTRACKER_H
+
+#include <s2e/Plugin.h>
+
+#include <s2e/Plugins/Core/BaseInstructions.h>
+
+namespace s2e
+{
+    namespace plugins
+    {
+
+        enum S2E_SQLITEFUNCTIONTRACKER_COMMANDS
+        {
+            // TODO: customize list of commands here
+            COMMAND_1
+        };
+
+        struct S2E_SQLITEFUNCTIONTRACKER_COMMAND
+        {
+            S2E_SQLITEFUNCTIONTRACKER_COMMANDS Command;
+            union
+            {
+                // Command parameters go here
+                uint64_t param;
+            };
+        };
+
+        class SqliteFunctionTracker : public Plugin, public IPluginInvoker
+        {
+
+            S2E_PLUGIN
+        public:
+            SqliteFunctionTracker(S2E *s2e) : Plugin(s2e)
+            {
+            }
+
+            void initialize();
+
+            // Allow the guest to communicate with this plugin using s2e_invoke_plugin
+            virtual void handleOpcodeInvocation(S2EExecutionState *state, uint64_t guestDataPtr, uint64_t guestDataSize);
+            void onCall(S2EExecutionState *state, const ModuleDescriptorConstPtr &source, const ModuleDescriptorConstPtr &dest,
+                        uint64_t callerPc, uint64_t calleePc, const FunctionMonitor::ReturnSignalPtr &returnSignal);
+            void onRet(S2EExecutionState *state, const ModuleDescriptorConstPtr &source, const ModuleDescriptorConstPtr &dest,
+                       uint64_t returnSite, uint64_t functionPc);
+
+        private:
+            uint64_t m_address;
+            typedef std::pair<std::string, std::vector<unsigned char>> VarValuePair;
+            typedef std::vector<VarValuePair> ConcreteInputs;
+
+            void generateTestCases(S2EExecutionState *state);
+            void writeSimpleTestCase(llvm::raw_ostream &os, const ConcreteInputs &inputs);
+        };
+
+    } // namespace plugins
+} // namespace s2e
+
+#endif // S2E_PLUGINS_SQLITEFUNCTIONTRACKER_H
